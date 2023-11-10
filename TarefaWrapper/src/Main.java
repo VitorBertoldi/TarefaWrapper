e80e7913669dcb44727272159989806b3d188d2b
VitorBertoldi <117694686+VitorBertoldi@users.noreply.github.com>
2023-11-10 17:16:45 -0300

    commit

---
+++ TarefaWrapper/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int num = sc.nextInt();
+
+        Integer num1 = num;
+        System.out.println(num1);
+    }
+}
